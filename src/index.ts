f14036d47e0ccca79193ad4c048aef78cd9b0940
Paweł Kochanek <pawel.kochanek@gmail.com>
2018-12-17 22:49:30 +0100

    Returned array of Maps

--- src/index.ts
+++ src/index.ts
@@ -19,15 +19,44 @@ function getFileContent(file: string): Promise<string> {
   });
 }
 
-function extractDependencies(
-  fileContent: Map<string, JSONObject>
-): Array<string> {
+async function extractDependencies(from: string): Promise<Array<string>> {
+  // TODO: add try catch
+  const file: string = await getFileContent(from);
+  const fileContent: Map<string, JSONObject> = Map(JSON.parse(file));
+
   const dependencies = fileContent.get('dependencies');
   const devDependencies = fileContent.get('devDependencies');
   return Object.keys(Object.assign({}, dependencies, devDependencies));
 }
 
-getFileContent('package.json')
-  .then((res: string): Map<string, JSONObject> => Map(JSON.parse(res)))
-  .then((res): Array<string> => extractDependencies(res))
-  .then(res => console.log(res[0]));
+async function prepareDependenciesData(): Promise<Array<string>> {
+  // TODO: add try catch
+  const projectDependencies: Array<string> = await extractDependencies(
+    'package.json'
+  );
+  const libData: Array<Promise<string>> = [];
+  projectDependencies.forEach(dependency => {
+    libData.push(getFileContent(`node_modules/${dependency}/package.json`));
+  });
+  return Promise.all(libData);
+}
+
+async function convertLibDataToMap(
+  libData: Promise<Array<string>>
+): Promise<Array<Map<string, JSONObject>>> {
+  const x = await libData;
+  const y: Array<Map<string, JSONObject>> = [];
+  x.forEach(z => {
+    y.push(Map(JSON.parse(z)));
+  });
+  return y;
+}
+
+convertLibDataToMap(prepareDependenciesData()).then(res =>
+  console.log(res[0].get('version'))
+);
+
+// getFileContent('package.json')
+//   .then((res: string): Map<string, JSONObject> => Map(JSON.parse(res)))
+//   .then((res): Array<string> => extractDependencies(res))
+//   .then(res => console.log(res[0]));
